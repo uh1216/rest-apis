cb7c1852dfb71c32c864a7be4ff3e9693409a62f
ruh2034@naver.com <ruh2034@naver.com>
2022-06-25 09:23:48 +0900

    CORS 수정

--- src/main/java/common/restapis/config/WebConfig.java
+++ src/main/java/common/restapis/config/WebConfig.java
@@ -11,7 +11,9 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://sanghabank.com:8080");
+        registry.addMapping("/**")
+                .allowedOrigins("http://sanghabank.com:8080")
+                .allowCredentials(true);
     }
 
     @Override
